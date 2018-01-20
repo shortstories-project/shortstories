5bfbd64cb75370775e40ac4615a12a12e473fa7b
shashkovdanil <shashkovdanil@gmail.com>
2018-01-20 21:50:08 +0500

    Modern webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,34 @@
-var path = require("path"),
-    webpack = require("webpack"),
-    StatsPlugin = require("stats-webpack-plugin"),
-    ExtractTextPlugin = require("extract-text-webpack-plugin");
+const { resolve } = require('path')
 
-var devServerPort = process.env.WEBPACK_DEV_SERVER_PORT,
-    devServerHost = process.env.WEBPACK_DEV_SERVER_HOST,
-    publicPath = process.env.WEBPACK_PUBLIC_PATH;
-
-var config = {
-  target: 'node',
+module.exports = {
   entry: {
-    admin: "./apps/admin/assets/javascripts/main.js"
+    admin: './apps/admin/assets/javascripts/main.js'
   },
-
   output: {
-    path: path.join(__dirname, "public"),
-    filename: "[name]-[chunkhash].js"
+    path: resolve(__dirname, 'public'),
+    filename: '[name]-[chunkhash].js'
   },
-
-  resolve: {
-    root: path.join(__dirname, "apps")
-  },
-
-  plugins: [
-    new StatsPlugin("webpack_manifest.json")
-  ],
-
-  loaders: [
-    {
-      test: /\.scss$/,
-      loaders: ["style-loader", "css-loader", "sass-loader"]
-    }
-  ],
-
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015']
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
-    },
-    {
-      test: /\.scss$/,
-      loaders: ["style-loader", "css-loader", "sass-loader"]
-    },
-    { test: /\.json$/, loader: "json-loader" }
     ]
-  }
-};
-
-if (process.env.INBUILT_WEBPACK_DEV_SERVER === 'true') {
-  config.devServer = {
-    port: devServerPort,
-    headers: { "Access-Control-Allow-Origin": "*" }
-  };
-  config.output.publicPath = "//" + devServerHost + ":" + devServerPort + "/";
-}
-
-if (process.env.INBUILT_WEBPACK_DEV_SERVER === 'false') {
-  // config.plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }))
-  config.plugins.push(new ExtractTextPlugin("[name].css"))
-  config.loaders = [
-    {
-      test: /\.s?css$/,
-      loader: ExtractTextPlugin.extract("style-loader", "css!sass")
-    }
-  ]
-  config.module.loaders = [{
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'babel',
-    query: {
-      presets: ['es2015']
-    }
   },
-  {
-    test: /\.s?css$/,
-    loader: ExtractTextPlugin.extract("style-loader", "css!sass")
-  }]
 }
 
-module.exports = config;
