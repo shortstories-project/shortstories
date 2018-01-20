0c3d944acfd73050945b4ad3c92047b4fcea20fe
Mark Frost <cheerful.mf@gmail.com>
2018-01-20 19:01:48 +0500

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 var path = require("path"),
-    StatsPlugin = require("stats-webpack-plugin");
+    webpack = require("webpack"),
+    StatsPlugin = require("stats-webpack-plugin"),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var devServerPort = process.env.WEBPACK_DEV_SERVER_PORT,
     devServerHost = process.env.WEBPACK_DEV_SERVER_HOST,
     publicPath = process.env.WEBPACK_PUBLIC_PATH;
 
 var config = {
-  entry: {},
+  target: 'node',
+  entry: {
+    admin: "./apps/admin/assets/javascripts/main.js"
+  },
 
   output: {
     path: path.join(__dirname, "public"),
@@ -19,15 +24,62 @@ var config = {
 
   plugins: [
     new StatsPlugin("webpack_manifest.json")
-  ]
+  ],
+
+  loaders: [
+    {
+      test: /\.scss$/,
+      loaders: ["style-loader", "css-loader", "sass-loader"]
+    }
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    },
+    {
+      test: /\.scss$/,
+      loaders: ["style-loader", "css-loader", "sass-loader"]
+    },
+    { test: /\.json$/, loader: "json-loader" }
+    ]
+  }
 };
 
-if (process.env.INBUILT_WEBPACK_DEV_SERVER) {
+if (process.env.INBUILT_WEBPACK_DEV_SERVER === 'true') {
   config.devServer = {
     port: devServerPort,
     headers: { "Access-Control-Allow-Origin": "*" }
   };
   config.output.publicPath = "//" + devServerHost + ":" + devServerPort + "/";
+}
+
+if (process.env.INBUILT_WEBPACK_DEV_SERVER === 'false') {
+  // config.plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }))
+  config.plugins.push(new ExtractTextPlugin("[name].css"))
+  config.loaders = [
+    {
+      test: /\.s?css$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css!sass")
+    }
+  ]
+  config.module.loaders = [{
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel',
+    query: {
+      presets: ['es2015']
+    }
+  },
+  {
+    test: /\.s?css$/,
+    loader: ExtractTextPlugin.extract("style-loader", "css!sass")
+  }]
 }
 
 module.exports = config;
