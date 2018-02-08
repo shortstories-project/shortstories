54cb8cb0e9840c03c2d7c34b0935e70370496211
Mark Frost <cheerful.mf@gmail.com>
2018-02-08 23:09:38 +0500

    added list of stories for current_user

--- backend/apps/web_api/controllers/stories/create.rb
+++ backend/apps/web_api/controllers/stories/create.rb
@@ -2,14 +2,17 @@ module WebApi::Controllers::Stories
   class Create
     include WebApi::Action
 
+    before :authenticate!
+
     params do
       required(:text).filled(:str?)
     end
-  
+
     def call(params)
       repository = StoryRepository.new
-      if params.valid?
-        record = repository.create(params.to_h)
+
+      if params.valid? && current_user
+        record = repository.create(params.to_h.merge(user_id: current_user.id))
         status 200, JSON.generate(record.to_h)
       else
         status 422, JSON.generate(error: 'not valid')
---
+++ backend/apps/web_api/serializers/profile/stories/index.rb
@@ -0,0 +1,10 @@
+module WebApi::Serializers
+  module Profile::Stories
+    class Index < Hanami::Serializer::Base
+      attribute :id, Types::Int
+      attribute :text, Types::String
+      attribute :created_at, Types::DateTime
+      attribute :updated_at, Types::DateTime
+    end
+  end
+end
