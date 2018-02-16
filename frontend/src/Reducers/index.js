df1bea2192d8a7aa2ef651cd4a645f00d2e1d43c
Mark Frost <cheerful.mf@gmail.com>
2018-02-16 17:15:00 +0500

    add index to Reducers

---
+++ frontend/src/Reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'	
+import { routerReducer } from 'react-router-redux'	
+
+import entities from './Entities'	
+import UI from './UI'	
+
+export default combineReducers({	
+  UI,
+  entities,
+  routing: routerReducer,
+})
