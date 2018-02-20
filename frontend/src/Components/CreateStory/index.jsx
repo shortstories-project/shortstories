3b7f1af81f209490bd2cb57c6ba03b7cf1e41428
shashkovdanil <shashkovdanil@gmail.com>
2018-02-21 01:36:31 +0500

    Change text editor

--- frontend/src/Components/CreateStory/index.jsx
+++ frontend/src/Components/CreateStory/index.jsx
@@ -2,13 +2,10 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
-import Quill from 'quill'
-
 import Button from '../Commons/Button'
 import Preloader from '../Commons/Preloader'
-
+import StoryEditor from '../Commons/StoryEditor/StoryEditor'
 import { createStory } from '../../Actions/Commons'
-
 import { conditionalRender } from '../../Utils'
 
 const Wrapper = styled.main`
@@ -19,58 +16,24 @@ const Wrapper = styled.main`
   min-width: 640px;
 `
 
-const Count = styled.p`
-  text-align: right;
-`
-
 type Props = {
   dispatch: (func: Function) => {},
-  isFetching: boolean
+  isFetching: boolean,
 }
 
-type State = {
-  story: string
-}
-
-class CreateStory extends Component<any, Props, State> {
-  quill;
-  state = {
-    story: ''
-  }
-
+class CreateStory extends Component<any, Props, any> {
   componentDidMount() {
-    const fonts = ['pt-sans', 'roboto', 'montserrat', 'noto-sans', 'marck-script', 'amatic-sc']
-    const Font = Quill.import('formats/font')
-    Font.whitelist = fonts
-    Quill.register(Font, true)
-    this.quill = new Quill('#editor', {
-      modules: {
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          [{ font: fonts }]
-        ]
-      },
-      placeholder: 'Where is my mind...',
-      theme: 'snow'
-    })
-    this.quill.format('font', 'pt-sans')
-  }
-
-  writeStory = (e: SyntheticInputEvent) => {
-    this.setState({ story: e.target.value })
   }
 
   render() {
-    const { story } = this.state
     const { isFetching, dispatch } = this.props
     return (
       <Wrapper>
-        <Count>{3000 - story.length}</Count>
-        <div id="editor" />
+        <StoryEditor />
         <Button
           onClick={() => {
-            dispatch(createStory(this.quill.container.firstChild.innerHTML))
+            console.log()
+            // dispatch()
           }}
         >
           {conditionalRender(isFetching, <Preloader />, 'Publish your story')}
@@ -81,7 +44,7 @@ class CreateStory extends Component<any, Props, State> {
 }
 
 const mapStateToProps = ({ UI }: Object) => ({
-  isFetching: UI.createStoryFetching
+  isFetching: UI.createStoryFetching,
 })
 
 export default connect(mapStateToProps)(CreateStory)
