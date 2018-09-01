7163d0a392ccb7377a1e70fbb570d07d5a01ed6a
shashkovdanil <shashkovdanil@gmail.com>
2018-09-01 21:20:52 +0500

    Add responsive

--- packages/client/src/pages/stories/index.tsx
+++ packages/client/src/pages/stories/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { Query } from 'react-apollo'
+import Measure from 'react-measure'
 import gql from 'graphql-tag'
 import { Grid } from 'mauerwerk'
 import { Story } from 'components'
@@ -31,28 +32,60 @@ const StyledGrid = styled(Grid)`
   height: calc(100vh - 80px) !important;
 `
 
+// For responsive
+const getColumns = (width: number): number => {
+  if (width >= 1200) {
+    return 4
+  } else if (width < 1200 && width >= 992) {
+    return 3
+  } else if (width < 992 && width >= 768) {
+    return 2
+  }
+  return 1
+}
+
 class StoriesList extends React.PureComponent<any> {
+  public state = {
+    dimensions: {
+      width: -1,
+    },
+  }
+
   public componentDidMount() {
     this.props.refetch()
   }
 
   public render() {
     const { loadMore, hasNextPage, stories } = this.props
+    const { width } = this.state.dimensions
     return (
       <main>
-        <StyledGrid
-          data={stories}
-          keys={(d: IStory) => d.id}
-          columns={4}
-          heights={(d: IStory) => d.body.length / 3}
-          margin={20}
-          lockScroll
-          closeDelay={250}
-        >
-          {(data: any, maximized: any, toggle: any) => {
-            return <Story {...data} maximized={maximized} toggle={toggle} />
+        <Measure
+          bounds
+          onResize={contentRect => {
+            this.setState({ dimensions: contentRect.bounds })
           }}
-        </StyledGrid>
+        >
+          {({ measureRef }) => (
+            <div ref={measureRef}>
+              <StyledGrid
+                data={stories}
+                keys={(d: IStory) => d.id}
+                columns={getColumns(width) || 4}
+                heights={(d: IStory) => d.body.length / 3}
+                margin={20}
+                lockScroll
+                closeDelay={250}
+              >
+                {(data: any, maximized: any, toggle: any) => {
+                  return (
+                    <Story {...data} maximized={maximized} toggle={toggle} />
+                  )
+                }}
+              </StyledGrid>
+            </div>
+          )}
+        </Measure>
         {hasNextPage && <button onClick={loadMore}>MORE</button>}
       </main>
     )
