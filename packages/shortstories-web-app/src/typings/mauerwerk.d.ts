62c05baa80fcb762c7c206bcad4ac55847e3e056
shashkovdanil <shashkovdanil@gmail.com>
2018-09-28 20:46:55 +0500

    Update deps and add git hooks

--- packages/shortstories-web-app/src/typings/mauerwerk.d.ts
+++ packages/shortstories-web-app/src/typings/mauerwerk.d.ts
@@ -1,6 +1,6 @@
-declare module "mauerwerk" {
-  const Grid: any;
-  const Slug: any;
-  const Fade: any;
-  export { Grid, Slug, Fade };
+declare module 'mauerwerk' {
+  const Grid: any
+  const Slug: any
+  const Fade: any
+  export { Grid, Slug, Fade }
 }
