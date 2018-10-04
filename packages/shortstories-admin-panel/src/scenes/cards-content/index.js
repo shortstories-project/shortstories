4a453bc0bd51b937de50ad863f0a988594e22304
migovan <mr.migovan@mail.ru>
2018-10-04 12:25:05 +0500

    setting router, made mark up pages - enter, sign-in and sign-up

---
+++ packages/shortstories-admin-panel/src/scenes/cards-content/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import 'bulma/css/bulma.css'
+// import { Link } from 'react-router-dom'
+
+
+class ChooseContent extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+          <p>dfesfdsf</p>
+      </div>
+    )
+  }
+}
+
+export default ChooseContent
---
+++ packages/shortstories-admin-panel/src/scenes/users/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Query } from "react-apollo";
+import {GET_STORIES} from "../../constants/queries";
+
+const Stories = () => (
+  <Query query={GET_STORIES}>
+    {({ data, loading}) => {
+      const { stories } = data
+
+      if (loading || !stories) {
+        return <p>loading...</p>
+      }
+    }}
+  </Query>
+)
+
+export default Stories
