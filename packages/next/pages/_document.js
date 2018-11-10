5aba4b5d87d98d48c2accd299bbab60ae7cda274
shashkovdanil <shashkovdanil@gmail.com>
2018-11-10 22:17:15 +0500

    Fix _document

--- packages/next/pages/_document.js
+++ packages/next/pages/_document.js
@@ -1,31 +1,3 @@
-// import Document, { Head, Main, NextScript } from 'next/document'
-// import { ServerStyleSheet } from 'styled-components'
-
-// class MyDocument extends Document {
-//   static getInitialProps({ renderPage }) {
-//     const sheet = new ServerStyleSheet()
-//     const page = renderPage(App => props =>
-//       sheet.collectStyles(<App {...props} />)
-//     )
-//     const styleTags = sheet.getStyleElement()
-//     return { ...page, styleTags }
-//   }
-
-//   render() {
-//     return (
-//       <html lang="en">
-//         <Head>{this.props.styleTags}</Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </html>
-//     )
-//   }
-// }
-
-// export default MyDocument
-
 import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
